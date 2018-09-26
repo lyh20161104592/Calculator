c393c9e860b4c8f1d610573e1e1cfaec1862cb34
20131104592 <lyh@admindeiMac-62.local>
2018-09-26 12:08:29 +0800

    1
    1

---
+++ text921/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  text921
+//
+//  Created by LYH on 2018/9/21.
+//  Copyright © 2018年 LYH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+   
+    @IBAction func num1(_ sender: Any) {
+        num1.Display
+    }
+
+    @IBAction func num2(_ sender: Any) {
+    }
+    
+    @IBAction func num3(_ sender: Any) {
+    }
+    
+    @IBAction func num4(_ sender: Any) {
+    }
+    
+    @IBAction func num5(_ sender: Any) {
+    }
+    
+    @IBAction func num6(_ sender: Any) {
+    }
+    
+    @IBAction func num7(_ sender: Any) {
+    }
+    
+    @IBAction func num8(_ sender: Any) {
+    }
+    
+    @IBAction func num9(_ sender: Any) {
+    }
+    
+    @IBAction func num0(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var Display: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ text921Tests/text921Tests.swift
@@ -0,0 +1,34 @@
+//
+//  text921Tests.swift
+//  text921Tests
+//
+//  Created by LYH on 2018/9/21.
+//  Copyright © 2018年 LYH. All rights reserved.
+//
+
+import XCTest
+@testable import text921
+
+class text921Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
