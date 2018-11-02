6f360fb45eae5d2e8488d8f66ba144e42183981d
20131104592 <lyh@admindeiMac-62.local>
2018-11-02 17:41:29 +0800

    9
    9

--- text921/ViewController.swift
+++ text921/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     var add = 0
     var result = ""
     var judge = 0
+    var lyh = ""
     @IBAction func num1(_ sender: Any) {
         if re == 1{
             Display.text = "1"
@@ -113,6 +114,8 @@ class ViewController: UIViewController {
     }
     @IBAction func clear(_ sender: Any) {
         Display.text = ""
+        result = ""
+        re = 0
     }
    
     @IBAction func plus(_ sender: Any) {
@@ -236,20 +239,34 @@ class ViewController: UIViewController {
         else{
         let a = Double(result)!
         let b = Double(Display.text!)!
-        
          if number == 1{
-            let c = a + b
-            Display.text = String(format: "%.1f", c)
+            let c = ((a*1000000) + (b*1000000))/1000000
+            Display.text = String(c)
         }else if number == 2{
-            let c = a - b
+            let c = ((a*1000000) - (b*1000000))/1000000
             Display.text = String(c)
         }else if number == 3{
-            let c = a * b
+            let c = ((a*100000) * (b*100000))/10000000000
             Display.text = String(c)
         }else if number == 4{
-            let c = a / (b)
-            Display.text = String(c)
+            let c = (a*1000000)/(b*1000000)
+            if b == 0 {
+                Display.text = "不能除以零"
+            }else
+            {
+              Display.text = String(c)
+            }
         }
+        var lyh:String = Display.text!
+            while lyh.last == "0"{
+                lyh.removeLast()
+                print(lyh)
+            }
+            while lyh.last == "."{
+                lyh.removeLast()
+                print(lyh)
+            }
+        Display.text = lyh
         re = 1
         judge = 0
         add = 0
